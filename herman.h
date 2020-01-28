3939d4e0188d745786f1dbcf418d1a0934171af1
Maximilian Starke <maximilian.starke@medionmail.com>
2020-01-29 00:17:16 +0100

    review herman

--- herman.h
+++ herman.h
@@ -19,12 +19,13 @@
 	 @exception std::invalid_argument Markov chain must be empty.
 	 @exception std::invalid_argument Size of herman must be odd.
 	 @exception std::invalid_argument Size of herman is too big for storing all states in _Integers type.
-	 // @excpetion std::invalid_argument Decoration 0 needed for target set. ###
 	 @exception std::invalid_argument Reward 0 needed for costs.
  */
 template<class _Rationals, class _Integers, class _Set, bool disable_checks = false>
 std::chrono::nanoseconds generate_herman(markov_chain<_Rationals, _Integers>& mc, const _Integers& size, std::unique_ptr<_Set>& target_set) {
-	//### get rid of unique ptr
+	//### get rid of unique ptr ? // -> then make unique must be replace set must be cleared.
+
+	const _Integers INT1{ 1 };
 
 	// Compile-time checks:
 	static_assert(std::is_same<typename _Set::value_type, _Integers>::value,
@@ -34,8 +35,6 @@ std::chrono::nanoseconds generate_herman(markov_chain<_Rationals, _Integers>& mc
 	if constexpr (!disable_checks) {
 		const bool size_too_big = !
 			[](const _Integers& size) { // ~checks if shift does not exceed type _Integers
-			// ! _Integers is the type which is actually used ??
-			// ! std::size_t is the natural upperbound from architecture ??
 			_Integers shifted{ 1 };
 			for (_Integers i{ 0 }; i != size; ++i) {
 				const _Integers next{ shifted << 1 };
@@ -53,7 +52,6 @@ std::chrono::nanoseconds generate_herman(markov_chain<_Rationals, _Integers>& mc
 		if (!mc.empty()) throw std::invalid_argument("Markov chain must be empty.");
 		if (!(size % 2)) throw std::invalid_argument("Size of herman must be odd.");
 		if (size_too_big) throw std::invalid_argument("Size of herman is too big for storing all states in _Integers type.");
-		// if (mc.n_node_decorations == 0) throw std::invalid_argument("Decoration 0 needed for target set."); ###
 		if (mc.n_edge_decorations == 0) throw std::invalid_argument("Reward 0 needed for costs.");
 	}
 
@@ -62,15 +60,14 @@ std::chrono::nanoseconds generate_herman(markov_chain<_Rationals, _Integers>& mc
 
 	// Do the actual calculation:
 
-	// Initialize all states
+	// Initialize all states:
 	for (_Integers state{ 0 }; (state >> size) == 0; ++state) {
 		mc.init_state(state);
 	}
 
-	// Enumerates all states that are target:
-	const auto enumerator = [&](
+	const auto target_state_enumerator{ [&](
 		const _Integers& herman_size,
-		auto tell /* Callback function that is used to lof final states: tell(const _Integers&)*/
+		auto tell /* Callback function that is used to lof final states: tell(const _Integers&) */
 		) -> void {
 			const auto helper = [&](
 				const _Integers& max_pos /* max bit position that can be 1*/,
@@ -98,46 +95,48 @@ std::chrono::nanoseconds generate_herman(markov_chain<_Rationals, _Integers>& mc
 						else self(max_pos, node, pos + 1, false, this_bit, tell, self);
 					}
 			};
-			for (auto most_significant_bit : { true, false }) helper(herman_size - 1, 0, 0, true, most_significant_bit, tell, helper);
-	};
+			for (auto most_significant_bit : { true, false })
+				helper(herman_size - 1, 0, 0, true, most_significant_bit, tell, helper);
+	} };
 
-	// save target states
-	target_set = std::make_unique<std::unordered_set<unsigned long>>();
-	enumerator(size, [&](const _Integers& value) {
-		target_set->insert(value);
-		});
+	// Save target states:
+	target_set = std::make_unique<_Set>();
+	target_state_enumerator(size, [&](const _Integers& value) { target_set->insert(value); });
 
-	// for all states create outgoing edges:
-	for (std::size_t state{ 0 }; state < (1uLL << size); ++state) {
-		std::size_t next_state{ 0 };
-		std::vector<unsigned int> non_deterministic_positions{};
+	// For all states create outgoing edges...
+	for (_Integers state{ 0 }; state < (INT1 << size); ++state) {
+		_Integers next_state{ 0 };
+		std::vector<_Integers> non_deterministic_positions{};
 		non_deterministic_positions.reserve(size);
 
-		// list all non-det positions and set deterministic ones of nex_state:
-		for (unsigned int pos{ 0 }; pos < size; ++pos) {
+		// List all non-det positions and set deterministic ones of nex_state:
+		for (_Integers pos{ 0 }; pos < size; ++pos) {
 			// bits in from state:
-			bool current_bit = state & (1uLL << pos);
-			bool next_bit = state & (1uLL << (pos + 1) % size);
+			bool current_bit = state & (INT1 << pos);
+			bool next_bit = state & (INT1 << (pos + 1) % size);
 
 			bool is_det{ current_bit != next_bit };
-			if (is_det) next_state |= static_cast<std::size_t>(next_bit) << pos;
+			if (is_det) next_state |= _Integers(next_bit) << pos;
 			else non_deterministic_positions.push_back(pos);
 		}
-		// probability of going to next state
-		double prob = 1.0 / (1ull << non_deterministic_positions.size());
 
-		// for all outgoing edges:
-		for (auto i = 0uLL; i < 1ull << non_deterministic_positions.size(); ++i) {
-			//map bits of i to the non_deterministic_positions:
-			std::size_t c_next_state{ next_state };
-			for (std::size_t pos_it = 0; pos_it < non_deterministic_positions.size(); ++pos_it) {
-				bool bit_to_map = (i & 1ull << pos_it);
-				c_next_state |= static_cast<unsigned long long>(bit_to_map) << non_deterministic_positions[pos_it];
+		_Rationals unique_outgoing_edge_probability = 1.0 / (INT1 << non_deterministic_positions.size());
+
+		// For all outgoing edges:
+		for (_Integers bits_for_nondet_positions{ 0 };
+			bits_for_nondet_positions < INT1 << non_deterministic_positions.size();
+			++bits_for_nondet_positions)
+		{
+			//map bits to the non_deterministic_positions:
+			_Integers next_state_copy{ next_state };
+			for (_Integers pos_it{ 0 }; pos_it < non_deterministic_positions.size(); ++pos_it) {
+				bool bit_to_map = (bits_for_nondet_positions & INT1 << pos_it);
+				next_state_copy |= static_cast<unsigned long long>(bit_to_map) << non_deterministic_positions[pos_it];
 			}
 			// create edge:
-			const auto pEdge{ new markov_chain<double>::edge(prob, mc.n_edge_decorations) };
-			mc.forward_transitions[state][c_next_state] = pEdge;
-			mc.inverse_transitions[c_next_state][state] = pEdge;
+			const auto pEdge{ new markov_chain<double>::edge(unique_outgoing_edge_probability, mc.n_edge_decorations) };
+			mc.forward_transitions[state][next_state_copy] = pEdge;
+			mc.inverse_transitions[next_state_copy][state] = pEdge;
 			pEdge->decorations[0] = 1;
 		}
 	}
