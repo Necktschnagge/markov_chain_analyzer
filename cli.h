a69cb762d182aba143e1eb949ba87c8ec4c5dab1
Maximilian Starke <maximilian.starke@medionmail.com>
2020-03-05 18:08:55 +0100

    diff

--- cli.h
+++ cli.h
@@ -78,19 +78,19 @@ inline nlohmann::json cli(std::istream& commands, global& g) {
 						id = std::stoull(items[1]);
 						n_state_decoration = std::stoull(items[2]);
 						n_transition_decoration = std::stoull(items[3]);
-			}
-			catch (...) { throw failed_instruction("Could not parse parameter."); }
-			g.markov_chains[id] = std::make_unique<mc_type>(n_transition_decoration, n_state_decoration);
-			performance_log.push_back({
-					{instruction,
-						{
-							{ sc::markov_chain_id, id},
-							{ sc::number_node_decorations, n_state_decoration},
-							{ sc::number_edge_decorations, n_transition_decoration}
+				}
+				catch (...) { throw failed_instruction("Could not parse parameter."); }
+				g.markov_chains[id] = std::make_unique<mc_type>(n_transition_decoration, n_state_decoration);
+				performance_log.push_back({
+						{instruction,
+							{
+								{ sc::markov_chain_id, id},
+								{ sc::number_node_decorations, n_state_decoration},
+								{ sc::number_edge_decorations, n_transition_decoration}
+							}
 						}
-					}
-				});
-			continue;
+					});
+				continue;
 		}
 
 		if (instruction == cli_commands::READ_TRA) {
