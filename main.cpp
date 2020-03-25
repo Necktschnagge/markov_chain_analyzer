c7cfbfb00c79152dfe35db832ed889ba6dd7066b
Maximilian Starke <maximilian.starke@medionmail.com>
2020-03-25 16:16:10 +0100

    remove old diagram generation suff not used anymore

--- main.cpp
+++ main.cpp
@@ -8,7 +8,6 @@
 #include "nlohmann/json.hpp"
 
 static constexpr bool DEBUG_MODE = false;
-static constexpr bool COMPRESSED_JSON_MODE = false;
 
 struct cli_params {
 
@@ -85,69 +84,6 @@ int main(int argc, char** argv)
 	if (!params.instructions) params.instructions = &std::cin;
 	performance_log = std::move(cli(*params.instructions, g));
 	if (params.json_log)  *params.json_log << performance_log;
-
-	/* prepare diagram output */
-	if constexpr (COMPRESSED_JSON_MODE) {
-		auto compressed{ nlohmann::json() };
-		int pos = -1;
-
-		for (auto it = performance_log.cbegin(); it != performance_log.cend(); ++it)
-		{
-			std::cout << "\n\n" << *it;
-
-			if (it->contains(cli_commands::GENERATE_HERMAN)) {
-				++pos;
-				const auto& herman{ it->operator[](cli_commands::GENERATE_HERMAN) };
-				compressed[pos]["herman_size"] = herman["size"];
-				for (const auto& pair : herman.items()) compressed[pos][pair.key()] = pair.value();
-			}
-			if (it->contains(cli_commands::CALC_VARIANCE)) {
-				const auto& variance{ it->operator[](cli_commands::CALC_VARIANCE) };
-				for (const auto& pair : variance.items()) compressed[pos][pair.key()] = pair.value();
-			}
-		}
-		std::cout << "\n\n\n\n" << compressed << "\n\n";
-
-		std::stringstream total_time_of_herman_size;
-		std::stringstream les_solving_of_herman_size;
-
-		std::stringstream total_time_of_edges;
-		std::stringstream les_solving_of_edges;
-
-		std::stringstream les_solving_of_herman_relative;
-
-		std::stringstream states_of_herman_size;
-		std::stringstream edges_of_herman_size;
-
-
-		for (const auto& measure : compressed) {
-
-			total_time_of_herman_size << "(" << measure["size"] << "," << (measure["total_time"].operator double() + measure["generator"].operator double()) << ")";
-			les_solving_of_herman_size << "(" << measure["size"] << "," << (measure["linear_system_solve_time"].operator double()) << ")";
-
-			total_time_of_edges << "(" << measure["#edges"] << "," << (measure["total_time"].operator double() + measure["generator"].operator double()) << ")";
-			les_solving_of_edges << "(" << measure["#edges"] << "," << (measure["linear_system_solve_time"]) << ")";
-
-			les_solving_of_herman_relative << "(" << measure["size"] << "," << measure["linear_system_solve_time"].operator double() * 100.0 / (measure["total_time"].operator double() + measure["generator"].operator double()) << ")";
-
-			states_of_herman_size << "(" << measure["size"] << "," << (measure["#nodes"]) << ")";
-			edges_of_herman_size << "(" << measure["size"] << "," << (measure["#edges"]) << ")";
-
-
-		}
-
-		std::cout << "total_time_of_herman_size\n" << total_time_of_herman_size.str() << "\n";
-		std::cout << "les_solving_of_herman_size\n" << les_solving_of_herman_size.str() << "\n";
-
-		std::cout << "total_time_of_edges\n" << total_time_of_edges.str() << "\n";
-		std::cout << "les_solving_of_edges\n" << les_solving_of_edges.str() << "\n";
-
-		std::cout << "states_of_herman_size\n" << states_of_herman_size.str() << "\n";
-		std::cout << "edges_of_herman_size\n" << edges_of_herman_size.str() << "\n";
-
-
-		std::cout << "les_solving_of_herman_relative\n" << les_solving_of_herman_relative.str() << "\n";
-	}
 
 	std::cout << "Finished.\n";
 }
